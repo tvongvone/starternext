578696beadde90df9e661b4275e073972ee9ab40
Tony Vongvone <tvongvone@gmail.com>
2024-03-20 23:07:49 -0700

    templateg

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,11 @@ import React from 'react'
 const page = () => {
   return (
     <div>
+      <div>Navigation Component</div>
       <h1>Tony's page</h1>
       <div>This will be the homepage of this app</div>
+      <div>About me</div>
+
     </div>
   )
 }
