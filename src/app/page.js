d7215136a409a3bdb405850941136a96b13618ba
Tony Vongvone <tvongvone@gmail.com>
2024-03-21 23:06:15 -0700

    deleted divs

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,6 @@ const page = () => {
   return (
     <div>
       <div>Navigation Component</div>
-      <h1>Tony's page</h1>
-      <div>This will be the homepage of this app</div>
-      <div>About me</div>
-
     </div>
   )
 }
