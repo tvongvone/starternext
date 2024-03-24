289031071d094c1a782062f4fbc7e370803599f8
Tony Vongvone <tvongvone@gmail.com>
2024-03-23 22:33:00 -0700

    Portfolio

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from './About'
+
+function index() {
+  return (
+    <div className="root">
+        Hello there
+    <About />
+    </div>
+  )
+}
+
+export default index
